c7ebb23c051fd320e4747b2c7e691c7d813ca593
Lucas Josino <contact@lucasjosino.com>
2022-11-17 17:57:40 -0300

    Add `/repositories` and `/services`

--- src/main/java/com/lucasjosino/parkingcontrolapi/repositories/ParkingRepository.java
+++ src/main/java/com/lucasjosino/parkingcontrolapi/repositories/ParkingRepository.java
@@ -1,2 +1,11 @@
-package com.lucasjosino.parkingcontrolapi.repositories;public class ParkingRepository {
+package com.lucasjosino.parkingcontrolapi.repositories;
+
+import com.lucasjosino.parkingcontrolapi.models.ParkingModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingRepository extends JpaRepository<ParkingModel, UUID> {
 }
--- src/main/java/com/lucasjosino/parkingcontrolapi/services/ParkingService.java
+++ src/main/java/com/lucasjosino/parkingcontrolapi/services/ParkingService.java
@@ -1,2 +1,29 @@
-package com.lucasjosino.parkingcontrolapi.services;public class ParkingService {
+package com.lucasjosino.parkingcontrolapi.services;
+
+import com.lucasjosino.parkingcontrolapi.exceptions.ParkingNotFoundException;
+import com.lucasjosino.parkingcontrolapi.models.ParkingModel;
+import com.lucasjosino.parkingcontrolapi.repositories.ParkingRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ParkingService {
+    private final ParkingRepository parkingRepository;
+
+    public ParkingService(ParkingRepository parkingRepository) {
+        this.parkingRepository = parkingRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ParkingModel> findAll() {
+        return parkingRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public ParkingModel findById(UUID id) {
+        return parkingRepository.findById(id).orElseThrow(() -> new ParkingNotFoundException(id));
+    }
 }
